51048db6c346299669eafc5a764e47f5f7cf6cdd
Braden Oestreich <braden.i.oestreich@gmail.com>
2021-10-24 14:31:38 -0400

    improved loop

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ standard_set_uris = {
     'mid': 'https://api.scryfall.com/cards/search?order=set&q=e%3Amid&unique=prints',
 }
 
-set_uri = standard_set_uris['afr']
+set_uri = standard_set_uris['khm']
 
 set_json = requests.get(set_uri).json()
 cards = set_json['data']
@@ -19,14 +19,25 @@ cards_two = set_json_two['data']
 set_json_three = requests.get(set_json_two['next_page']).json()
 cards_three = set_json_three['data']
 
-tally = -1
+index = -1
 
 for card in cards:
     
-    tally += 1
+    index += 1
+    set_number = index + 1
 
-    print(str(tally) + ' ' + card['name'])
-    print(card['image_uris']['large'])
+    # print('Index: ' + str(index))
+    print(str(set_number) + ' ' + card['name'])
+
+    if 'card_faces' in card:
+        card_front = card['card_faces'][0]['image_uris']['normal']
+        card_back = card['card_faces'][1]['image_uris']['normal']
+        print(card_front)
+        print(card_back)
+    else:
+        print(card['image_uris']['normal'])
+
     print('\n')
 
-    # Unexpected problem: cards that have multiple faces have their 'image_uris' nested within a 'card_faces' key, so the logic will have to accomodate that.
+    # TODO: cards that have multiple faces have their 'image_uris' nested within a 'card_faces' key, so the logic will have to accomodate that.
+    # COMPLETE
